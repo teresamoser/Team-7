3d04314c5d4fe76a7b3a7b18c5c35b1cabca3f54
Teresa Moser <35321696+teresamoser@users.noreply.github.com>
2021-01-20 21:23:35 -0700

    Add files via upload

---
+++ cse210-tc03/jumper_template/jumper/__main__.py
@@ -0,0 +1,26 @@
+from game.director import Director
+
+director = Director()
+director.start_game()
+
+# Overview
+# Jumper is a game in which the player seeks to solve a puzzle by guessing 
+#   the letters of a secret word one at a time.
+
+# Rules
+# Jumper is played according to the following rules.
+# 1. The puzzle is a secret word randomly chosen from a list.
+# 2. The player guesses a letter in the puzzle.
+# 3. If the guess is correct, the letter is revealed.
+# 4. If the guess is incorrect, a line is cut on the player's parachute.
+# 5. Play continues until the puzzle is solved or the player has no more parachute.
+
+# Requirements
+# Your program must also meet the following requirements.
+# 1. The program must use the Jumper template.
+# 2. The program must have a README file with assignment and author names.
+# 3. The program must have at least four classes.
+# 4. Each module, class and method must have a corresponding comment using 
+#    the style demonstrated in the solo checkpoint.
+# 5. The game must remain generally true to the order of play described in 
+#    the rules.
