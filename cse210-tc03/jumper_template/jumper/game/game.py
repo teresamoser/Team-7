67079da1c3ff7cafead0568a6f353ea7581067d5
mtyra321 <mtyra321@gmail.com>
2021-01-21 18:17:30 -0700

    made classes in same file

--- cse210-tc03/jumper_template/jumper/game/game.py
+++ cse210-tc03/jumper_template/jumper/game/game.py
@@ -1,5 +1,4 @@
-import importlib
-import words
+#the game class will compile all the other classes and display the final game
 class Game:
 
     
@@ -15,7 +14,7 @@ if __name__ == "__main__":
 
 
 
-    #this class will choose the words that are needed for the jumper game
+    #the word class will choose the words that are needed for the jumper game
 
 class Words:
     def __init__(self):
@@ -23,4 +22,18 @@ class Words:
     def dealWord(self):
         print("deal word")
         return self.words.random.randint(0,len(self.words)-1)
+
+
+
+
+# the player class will have the number of lives and the number of attempts the user has done
+class Player:
+    def __init__(self):
+        lives = 5
+        attempts = 0
+    def wrong_attempt(self):
+        self.lives = self.lives -1
+        self.attempts = self.attemps + 1
+
+
 
