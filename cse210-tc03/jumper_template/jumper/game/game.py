b9cd926f8e9fab5ae53e53b835a6a6d15e1e2ddd
mtyra321 <mtyra321@gmail.com>
2021-01-21 18:00:01 -0700

    put the classes in one file

--- cse210-tc03/jumper_template/jumper/game/game.py
+++ cse210-tc03/jumper_template/jumper/game/game.py
@@ -1,12 +1,26 @@
 import importlib
-moduleName = input('words')
-importlib.import_module(moduleName)
-class game:
+import words
+class Game:
 
     
     def start_game(self):
         words = Words()
+        words.dealWord()
 
 if __name__ == "__main__":
-    hilo = HiLo()
-    hilo.start_game()
+    game = Game()
+    game.start_game()
+
+
+
+
+
+    #this class will choose the words that are needed for the jumper game
+
+class Words:
+    def __init__(self):
+        words = ["test", "something", "chicken"]
+    def dealWord(self):
+        print("deal word")
+        return self.words.random.randint(0,len(self.words)-1)
+
