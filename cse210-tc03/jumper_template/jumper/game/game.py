79a011a6391a14c38af69dcaa2bc946548c3d542
Teresa <moserte@byui.edu>
2021-01-23 20:45:36 -0700

    Clean up
    just clean up on spaces, etc. forward slash changed to backslash.

--- cse210-tc03/jumper_template/jumper/game/game.py
+++ cse210-tc03/jumper_template/jumper/game/game.py
@@ -2,29 +2,29 @@
 import random
 import sys
 
-DEFAULT_VALUE = "jumper_template\jumper\game\words.txt"
+DEFAULT_VALUE = "jumper_template/jumper/game/words.txt"
 FILENAME = "words.txt" # sys.argv[1] if len(sys.argv) == 2 else DEFAULT_VALUE
 class Game: 
     def start_game(self):
         player = Player()
         words = Words()
-        theword =words.dealWord()
+        theword = words.dealWord()
         display = Display(theword, player)
         keepGoing = True
         print("the Word is", theword)
-        while player.lives >0 and keepGoing == True:
+        while player.lives > 0 and keepGoing == True:
             guess = input("Guess a letter [a-z] or input ! to quit: ")
             while len(guess) > 1:
                 print("Please only input one letter")
                 guess = input("Guess a letter [a-z]: ")
-            print("your input was ", guess)
+                print("your input was ", guess)
             if guess == "!":
                 keepGoing = False
                 break
             for letter in theword:
                 if letter == guess:
                     display.foundLetter(letter)
-            display.display()
+                    display.display()
         
 
 # display will handle the display part of the game
@@ -44,6 +44,7 @@ class Display:
         print()
         if self.player.lives > 4:
             print(" _____")
+
         if self.player.lives > 3:
             print("/_____\\")
 
@@ -56,27 +57,27 @@ class Display:
         if self.player.lives > 0:
             print("  \\ /")
             print("   0")
+
         if self.player.lives == 0:
             print("   x")
-        print("  /|\\")
-        print("  / \\")
-        print("^^^^^^^^^^^")
+            print("  /|\\")
+            print("  / \\")
+            print("^^^^^^^^^^^")
+
     def foundLetter(self, letter):
         self.letters.append(letter)
 
 
-
-    #the word class will choose the words that are needed for the jumper game
+ #the word class will choose the words that are needed for the jumper game
 class Words:
     def __init__(self):
-        file = open(FILENAME)       #open file
+        file = open(FILENAME)     #open file
         dataString = file.read()  #turn file into string
         self.wordlist = dataString.split("\n")
+
     def dealWord(self):
         print("deal word")
-        return self.wordlist[random.randint(0,len(self.wordlist)-1)]
-
-
+        return self.wordlist[random.randint(0, len(self.wordlist)-1)]
 
 
 # the player class will have the number of lives and the number of attempts the user has done
@@ -86,11 +87,11 @@ class Player:
         self.attempts = 0
         self.correct_counter = 0
         self.guesses = []
+
     def wrong_attempt(self):
         self.lives = self.lives -1
         self.attempts = self.attempts + 1
     
-
 
 if __name__ == "__main__":
     game = Game()
