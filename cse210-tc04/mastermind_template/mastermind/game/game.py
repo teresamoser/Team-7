9e13a076df4e2eca305f0403af307954dee3bf10
mtyra321 <mtyra321@gmail.com>
2021-01-26 12:56:23 -0700

    gets player name and the number

--- cse210-tc04/mastermind_template/mastermind/game/game.py
+++ cse210-tc04/mastermind_template/mastermind/game/game.py
@@ -4,8 +4,15 @@ import random
 
 #game class will compile all the other classes and call them appropriately
 class Game:
-    def __init__(self):
-        print()
+    def start_game(self):
+        self.player1 = Player(input("Enter a name for player 1:"))
+        self.player2 = Player(input("Enter a name for player 2:"))
+        display = Display()
+        self.number = Number()
+        self.turn = 1
+        self.guesser = Guessing()
+        print("Number is ", game.number.value)
+        display.displayGuess(1)
 
 
 
@@ -14,6 +21,8 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.attempts = 0
+        self.lastguess = "----"
+        self.lastguessOutput = "****"
         
 
 
@@ -21,12 +30,12 @@ class Player:
 #number class will deal a number
 class Number:
     def __init__(self):
-        length = 4
-        value = random.randint(1000,9999) # could change numbers based on lengths
+        self.length = 4
+        self.value = random.randint(1000,9999) # could change numbers based on lengths
 
 #the guessing class will prompt the both users to guess and send their guess to guessOutput
 class Guessing:
-    def __init__(self):
+    def promptGuess(self):
         print()
 
 #guessoutput will get called by guessing. It will take a single player's guess and validate it
@@ -37,7 +46,17 @@ class GuessOutput:
 #display will get called by guessing and will display the appropriate responses
 class Display:
     def __init__(self):
-        print()
+
+        print("------------------")
+        print("Player ", game.player1.name, ": ----, ****")
+        print("Player ", game.player2.name, ": ----, ****")
+        print("------------------")
+    def displayGuess(self, turn):
+        print("------------------")
+        print("Player ", game.player1.name, ": ",game.player1.lastguess, game.player1.lastguessOutput)
+        print("Player ", game.player2.name, ": ",game.player2.lastguess, game.player2.lastguessOutput)
+        print("------------------")       
+
 
 
 if __name__ == "__main__":
