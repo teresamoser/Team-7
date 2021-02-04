b382733ad0805550171baf3669752ae608a9d13b
mtyra321 <mtyra321@gmail.com>
2021-02-04 14:06:21 -0700

    added actor class from snake

--- cse210-tc05/speed_template/speed/game/director.py
+++ cse210-tc05/speed_template/speed/game/director.py
@@ -3,5 +3,7 @@ class Director:
     def __init__(self, input_ser, output_ser):
         input_service = input_ser
         output_service = output_ser
+        keep_playing = True
+        # make deault player, words
     def start_game(self):
         print()
