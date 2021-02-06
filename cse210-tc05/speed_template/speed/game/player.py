bd444394566898b652ad8ffa9c5691e837abfeac
bethanysteiner <77858640+bethanysteiner@users.noreply.github.com>
2021-02-06 15:12:17 -0700

    Player class and Score class

--- cse210-tc05/speed_template/speed/game/player.py
+++ cse210-tc05/speed_template/speed/game/player.py
@@ -1,5 +1,23 @@
 
 #will have a score and a buffer
+class Score:
+    """
+    Lets the player know they typed the right word and updates the score.
+    """
+    def __init__(self):
+        self.score = 0
+
+    def _rightword(self, playerword, wordArray):
+
+        if playerword in wordArray:
+            print ('Correct!')
+            self.score += len(playerword)
+        else:
+            print ('Incorrect.')        
+
+
+        
+
 class Player:
     """
 
@@ -15,5 +33,22 @@ class Player:
         Args:
             
         """
-        super().__init__()
+        self.word = ''
+        self.score = Score()
+    
+    def _addletter(self, letter):
+        if not letter == '*'
+            self.word += letter
+
+    def clearword(self):
+        self.word = ''
+
+    def matchword(self, wordArray):
+        self.score._rightword(self.word, wordArray)
+        self.word = ''
+
+
+
+        
+
         
