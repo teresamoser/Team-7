db45fa09976ca94742ba8aec8a61830ab6ed6ff1
Teresa Moser <35321696+teresamoser@users.noreply.github.com>
2021-02-06 21:53:00 -0700

    fixed "from game.word import Word"

--- cse210-tc05/speed_template/speed/game/director.py
+++ cse210-tc05/speed_template/speed/game/director.py
@@ -1,7 +1,7 @@
 # Teresa Moser responsible for entry in Class Director
 from time import sleep
 from game import constants
-from word import Word
+from game.word import Word
 from player import Player
 from score import Score
 
