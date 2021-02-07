2a925a2eece2a97f1aaadffeafd1e45683c2ec05
Teresa Moser <35321696+teresamoser@users.noreply.github.com>
2021-02-06 20:16:26 -0700

    Teresa made updates to director.py
    I have made the finishing touches, not sure if it runs. I am having issues with my visual studio, it won't do the imports. Please try and run it and see if it works!

--- cse210-tc05/speed_template/speed/game/director.py
+++ cse210-tc05/speed_template/speed/game/director.py
@@ -1,106 +1,85 @@
-# Teresa Moser--responsible for the edits to the class Director
-
+# Teresa Moser responsible for entry in Class Director
 from time import sleep
-import constants
+from game import constants
 from word import Word
 from player import Player
-# from snake import Snake
-
+from score import Score
 
 class Director:
     """A code template for a person who directs the game. The responsibility of 
-       this class of objects is to control the sequence of play.
-    
-    Stereotype:
-            Controller
-            
+        this class of objects is to control the sequence of play.
+        
+     Stereotype:
+        Controller
+        
     Attributes:
-         input_service (InputService): The input mechanism.
-         keep_playing (boolean): Whether or not the game can continue.
-         output_service (OutputService): The output mechanism.
+        input_service (InputService): The input mechanism.
+        output_service (OutputService): The output mechanism.
+        keep_playing (boolean): Whether or not the game can continue.
     """
+
     def __init__(self, input_ser, output_ser):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
         self._input_service = input_ser
         self._output_service = output_ser
         self._keep_playing = True
         self._player = Player()
         self._words = Word()
-             # make default player, words
-            
+        # make default player, words
+
     def start_game(self):
-         """Starts the game loop to control the sequence of play.
+        """Starts the game loop to control the sequence of play.
         
-         Args:
+        Args:
              self (Director): an instance of Director.
-         """
-         while self._keep_playing:
-             self._get_inputs()
-             self._do_updates()
-             self._do_outputs()
-             sleep(constants.FRAME_LENGTH)
-                
-#         print()
-        
+        """
+        while self._keep_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+            sleep(constants.FRAME_LENGTH)
+       
     def _get_inputs(self):
-         """Gets the inputs at the beginning of each round of play. In this case,
+        """Gets the inputs at the beginning of each round of play. In this case,
          that means getting the words from player
 
-         Args:
-             self (Director): An instance of Director.
-         """
-         direction = self._input_service.get_word()
-#         self._snake.move_head(direction)
+        Args:
+            self (Director): An instance of Director.
+        """
+        direction = self._input_service.get_word()
+        self._words.move(direction)
 
     def _do_updates(self):
-         """Updates the important game information for each round of play. In 
+        """Updates the important game information for each round of play. In 
             this case, that means checking for a word and updating the points.
 
-         Args:
+        Args:
              self (Director): An instance of Director.
-         """
-#         self._handle_body_collision()
-#         self._handle_food_collision()
+        """
+        self._rightword(Player)
+        self._score(Score)
+
         
     def _do_outputs(self):
-#         """Outputs the important game information for each round of play. In 
-#         this case, that means checking if there are stones left and declaring 
-#         the winner.
+        """Outputs the important game information for each round of play. In 
+        this case, that means checking if there are words are correct and updated the score. 
 
-#         Args:
-#             self (Director): An instance of Director.
-#         """
-#         self._output_service.clear_screen()
-#         self._output_service.draw_actor(self._food)
-#         self._output_service.draw_actors(self._snake.get_all())
-#         self._output_service.draw_actor(self._score)
-#         self._output_service.flush_buffer()
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._output_service.clear_screen()
+        self._output_service.draw_actor(Score)
+        self._output_service.flush_buffer()
+        print()
 
-#     def _handle_body_collision(self):
-#         """Handles collisions between the snake's head and body. Stops the game 
-#         if there is one.
 
-#         Args:
-#             self (Director): An instance of Director.
-#         """
-#         head = self._snake.get_head()
-#         body = self._snake.get_body()
-#         for segment in body:
-#             if head.get_position().equals(segment.get_position()):
-#                 self._keep_playing = False
-#                 break
-
-#     def _handle_food_collision(self):
-#         """Handles collisions between the snake's head and the food. Grows the 
-#         snake, updates the score and moves the food if there is one.
-
-#         Args:
-#             self (Director): An instance of Director.
-#         """
-#         head = self._snake.get_head()
-#         if head.get_position().equals(self._food.get_position()):
-#             points = self._food.get_points()
-#             for n in range(points):
-#                 self._snake.grow_tail()
-#             self._score.add_points(points)
-#             self._food.reset() 
-            print()
+# Rules of the speed game
+# 1. The game begins with five words moving on the screen.
+# 2. The player tries to type the words they see on the screen.
+# 3. The player earns points for the words they type correctly.
+# 4. If the player presses the enter key their buffer is cleared.
+# 5. Play continues until the player presses the ESC key.
