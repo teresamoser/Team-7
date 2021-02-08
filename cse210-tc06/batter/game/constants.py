86f0670710b4897340566acd02a9fc96b3dd6dfb
mtyra321 <mtyra321@gmail.com>
2021-02-08 13:30:45 -0700

    added files from solo checkpoint

---
+++ cse210-tc06/batter/game/constants.py
@@ -0,0 +1,8 @@
+import os
+
+MAX_X = 80
+MAX_Y = 20
+FRAME_LENGTH = 0.1
+PATH = os.path.dirname(os.path.abspath(__file__))
+#MESSAGES = open(PATH + "/messages.txt").read().splitlines()
+ARTIFACTS = 30
