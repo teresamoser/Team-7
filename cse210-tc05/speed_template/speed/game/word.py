8d74453b556ceedf0d59a77c32009c898de5a191
mtyra321 <mtyra321@gmail.com>
2021-02-08 13:15:42 -0700

    added week 6 template

--- cse210-tc05/speed_template/speed/game/word.py
+++ cse210-tc05/speed_template/speed/game/word.py
@@ -5,9 +5,9 @@ from pathlib import Path
 class Word(Actor):
     def __init__(self):
         super(Word, self).__init__()
-        file_dir = Path(".")
+        file_dir = Path(__file__).parent
         #you have to be in the cse210-tc05 folder for the file to open
-        file = open(file_dir/"speed_template/speed/game/words.txt")       #open file     
+        file = open(file_dir/"words.txt")       #open file     
         dataString = file.read()  #turn file into string
         self.wordArray= dataString.split() #turn string into array
         self.value = self.wordArray[random.randint(0,len(self.wordArray)-1)]
