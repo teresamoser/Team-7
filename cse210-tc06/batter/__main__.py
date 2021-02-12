7aa943b06ccd9456312b82e9131981ecdd969467
Teresa <moserte@byui.edu>
2021-02-11 23:19:33 -0700

    Update __main__.py

--- cse210-tc06/batter/__main__.py
+++ cse210-tc06/batter/__main__.py
@@ -5,6 +5,7 @@ from game.actor import Actor
 from game.point import Point
 from game.player import Player
 from game.ball import Ball
+from game.brick import Brick
 from game.control_actors_action import ControlActorsAction
 from game.draw_actors_action import DrawActorsAction
 from game.handle_collisions_action import HandleCollisionsAction
